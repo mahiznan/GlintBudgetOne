34e229e808e810d0634fb75bae96e82af01f07e8
Rajeshkumar <rajeshmepco@gmail.com>
2026-05-24 04:01:06 +0800

    feat: enable Firestore offline persistence on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+// lib/main.dart
+// ignore_for_file: deprecated_member_use
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
@@ -9,5 +13,12 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  if (kIsWeb) {
+    // IndexedDB-backed offline cache for web. Silent catch: some browsers
+    // block IndexedDB (private mode, extensions), which is acceptable.
+    FirebaseFirestore.instance
+        .enablePersistence(const PersistenceSettings(synchronizeTabs: true))
+        .catchError((_) {});
+  }
   runApp(const ProviderScope(child: App()));
 }
